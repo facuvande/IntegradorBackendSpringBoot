cb7ebc851ce231321f9af36a85c0a9799b2db7a7
Facundo <facundovs84@hotmail.com.ar>
2024-01-25 23:13:35 -0300

    Modelado de clase Cliente

--- src/main/java/com/facuvande/integrador/model/Cliente.java
+++ src/main/java/com/facuvande/integrador/model/Cliente.java
@@ -1,5 +1,24 @@
 package com.facuvande.integrador.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Cliente {
+    
+    private Long id_cliente;
+    private String nombre;
+    private String apellido;
+    private String dni;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id_cliente, String nombre, String apellido, String dni) {
+        this.id_cliente = id_cliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
     
 }
