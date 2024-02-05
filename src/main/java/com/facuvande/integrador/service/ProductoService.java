fbb3e9bf303ade358160fb0fefcebf4c46119533
Facundo <facundovs84@hotmail.com.ar>
2024-02-05 20:12:06 -0300

    Creacion de Metodos

--- src/main/java/com/facuvande/integrador/service/ProductoService.java
+++ src/main/java/com/facuvande/integrador/service/ProductoService.java
@@ -1,5 +1,38 @@
 package com.facuvande.integrador.service;
 
-public class ProductoService {
+import com.facuvande.integrador.model.Producto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductoService implements IProductoService{
+
+    @Autowired
+    
+    @Override
+    public Producto getProductById(Long code) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Producto> getProducts() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void save(Producto product) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String deleteById(Long code) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Producto editProduct(Producto product) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
