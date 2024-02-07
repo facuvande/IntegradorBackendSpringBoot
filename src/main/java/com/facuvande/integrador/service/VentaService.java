5bea20b1e1fa32a8d74b009268328a61901913d1
Facundo <facundovs84@hotmail.com.ar>
2024-02-07 20:28:54 -0300

    Creacion de metodos en service

--- src/main/java/com/facuvande/integrador/service/VentaService.java
+++ src/main/java/com/facuvande/integrador/service/VentaService.java
@@ -1,8 +1,40 @@
 package com.facuvande.integrador.service;
 
+import com.facuvande.integrador.model.Venta;
+import com.facuvande.integrador.repository.IVentaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class VentaService {
+public class VentaService implements IVentaService{
+
+    @Autowired
+    private IVentaRepository saleRepository;
+    
+    @Override
+    public Venta getSaleById(Long code) {
+        return saleRepository.findById(code).orElse(null);
+    }
+
+    @Override
+    public List<Venta> getSales() {
+        return saleRepository.findAll();
+    }
+
+    @Override
+    public Venta save(Venta sale) {
+        return saleRepository.save(sale);
+    }
+
+    @Override
+    public void deleteById(Long code) {
+        saleRepository.deleteById(code);
+    }
+
+    @Override
+    public Venta editSale(Venta sale) {
+        return saleRepository.save(sale);
+    }
     
 }
